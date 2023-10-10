50b9a5bb2f40c5e728cf509e6e9836ba37b8c1c0
junyoung_hong <wnsdud0721@naver.com>
2023-10-10 19:30:18 +0900

    add gitignore

---
+++ IDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDo
+//
+//  Created by Junyoung_Hong on 2023/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
