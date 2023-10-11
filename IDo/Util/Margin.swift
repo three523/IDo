504d244949f571820400d5ac564410b878ce815b
junyoung_hong <wnsdud0721@naver.com>
2023-10-11 12:03:04 +0900

    [FEAT] TabBar 추가 & VC 추가

---
+++ IDo/Util/Margin.swift
@@ -0,0 +1,16 @@
+//
+//  Margin.swift
+//  IDo
+//
+//  Created by Junyoung_Hong on 2023/10/11.
+//
+
+import UIKit
+
+struct Constant {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let margin1: CGFloat = 4
+    static let margin2: CGFloat = 8
+    static let margin3: CGFloat = 16
+}
