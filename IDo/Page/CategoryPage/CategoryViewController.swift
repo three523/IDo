83627336d6ca770b1556ba2d51722f6019f231bd
KangJiHun1028 <minsakr10@naver.com>
2023-10-11 19:52:48 +0900

    [FEAT]: Cell 화면 전환 기능 구현

--- IDo/Page/CategoryPage/CategoryViewController.swift
+++ IDo/Page/CategoryPage/CategoryViewController.swift
@@ -7,49 +7,44 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController {
+class CategoryViewController: UITableViewController {
     let categoryData = ["IT/개발", "사진/영상", "음악/악기", "게임/오락", "여행/맛집", "댄스/공연", "동물/식물", "낚시/캠핑", "운동/스포츠"]
     let categoryImage = UIImage(systemName: "pencil")
-    private let tableView: UITableView = {
-        let tableview = UITableView()
-        return tableview
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
 
         tableView.register(CategoryCell.self, forCellReuseIdentifier: "Cell")
-        setConstraint()
 
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
     }
 
-    private func setConstraint() {
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-
-        tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(Constant.margin3)
-        }
-    }
-}
-
-extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryData.count
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CategoryCell
         cell.label.text = categoryData[indexPath.row]
         cell.categoryImageView.image = categoryImage
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 선택한 셀의 인덱스에 따라 다른 화면으로 전환
+        switch indexPath.row {
+        case 0:
+            let exampleVC = MeetingViewController()
+            navigationController?.pushViewController(exampleVC, animated: false)
+        default:
+            break
+        }
     }
 }
---
+++ IDo/Page/CategoryPage/MeetingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MeetingViewController.swift
+//  IDo
+//
+//  Created by t2023-m0053 on 2023/10/11.
+//
+
+import Foundation
+import UIKit
+
+class MeetingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
