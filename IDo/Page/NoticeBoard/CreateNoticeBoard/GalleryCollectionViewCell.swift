ce741e62893fddeb636918c87607abdac99b97ef
junyoung_hong <wnsdud0721@naver.com>
2023-10-12 15:50:30 +0900

    [RENAME] : ✏️ Basic Cell 파일 옮기기

---
+++ IDo/Page/NoticeBoard/CreateNoticeBoard/GalleryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryCollectionViewCell.swift
+//  IDo
+//
+//  Created by Junyoung_Hong on 2023/10/12.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+    // Cell 식별자
+    static let identifier = "GalleryCollectionViewCell"
+    
+}
