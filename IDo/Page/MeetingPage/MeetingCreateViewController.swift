64cf1f432a18f7911d13fe640723954fb565f261
direchan <hdjin92@gmail.com>
2023-10-12 10:39:44 +0900

    [FEAT] 모임 이름 설정 텍스트필드 추가

--- IDo/Page/MeetingPage/MeetingCreateViewController.swift
+++ IDo/Page/MeetingPage/MeetingCreateViewController.swift
@@ -3,7 +3,9 @@ import UIKit
 class MeetingCreateViewController: UIViewController {
     
     let profileImageButton = MeetingProfileImageButton()
-
+    let imageSetLabel = UILabel()
+    let meetingNameField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -12,11 +14,32 @@ class MeetingCreateViewController: UIViewController {
     private func configureUI() {
         // UI 설정
         view.addSubview(profileImageButton)
+        view.addSubview(imageSetLabel)
+        view.addSubview(meetingNameField)
         profileImageButton.addTarget(self, action: #selector(profileImageTapped), for: .touchUpInside)
         profileImageButton.snp.makeConstraints { (make) in
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(16)
             make.centerX.equalToSuperview()
         }
+        imageSetLabel.text = "대표 사진"
+        imageSetLabel.font = UIFont(name: "SF Pro", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .regular)
+        imageSetLabel.textAlignment = .center
+        imageSetLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(profileImageButton.snp.bottom).offset(12)
+            make.centerX.equalToSuperview()
+        }
+        meetingNameField.font = UIFont(name: "SF Pro", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .regular)
+        meetingNameField.borderStyle = .roundedRect
+        meetingNameField.backgroundColor = UIColor(named: "BackgroundSecondary")
+        meetingNameField.placeholder = "모임 이름을 설정하세요."
+        meetingNameField.snp.makeConstraints { (make) in
+            make.top.equalTo(imageSetLabel.snp.bottom).offset(12)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(361)
+            make.height.equalTo(37)
+        }
+        
+        
     }
     
     @objc private func profileImageTapped() {
@@ -34,10 +57,10 @@ extension MeetingCreateViewController: UIImagePickerControllerDelegate, UINaviga
     }
 }
 
-    
-    
-    
-    // 네비게이션
+
+
+
+// 네비게이션
 //    navigationController?.navigationBar.barTintColor = .white
 //    navigationItem.title = "미팅 생성"
 //    let cancelButton = UIBarButtonItem(title: "<", style: .plain, target: self, action: #selector(cancelButtonTapped))
