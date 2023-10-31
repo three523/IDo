664d6e5f9b84eecb61812ab8bd28f8e5283ce804
KangJiHun1028 <minsakr10@naver.com>
2023-10-31 17:27:08 +0900

    [FEAT]: 회원가입 화면 구현

--- IDo/Page/LoginPage/LoginView.swift
+++ IDo/Page/LoginPage/LoginView.swift
@@ -11,17 +11,17 @@ import UIKit
 class LoginView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         configureUI()
         addSubView()
         autoLayout()
     }
-    
+
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // 카카오 로그인 버튼
     private(set) lazy var kakaoLoginButton: UIButton = {
         let button = UIButton()
@@ -35,6 +35,7 @@ class LoginView: UIView {
         textField.font = .bodyFont(.medium, weight: .regular)
         textField.textColor = UIColor(color: .textStrong)
         textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
         return textField
     }()
 
@@ -44,7 +45,7 @@ class LoginView: UIView {
         textField.font = .bodyFont(.medium, weight: .regular)
         textField.textColor = UIColor(color: .textStrong)
         textField.borderStyle = .roundedRect
-        textField.isSecureTextEntry = true 
+        textField.isSecureTextEntry = true
         return textField
     }()
 
@@ -72,7 +73,7 @@ private extension LoginView {
     func configureUI() {
         backgroundColor = UIColor.white
     }
-    
+
     // noticeBoardTableView를 SubView에 추가
     func addSubView() {
 //        addSubview(kakaoLoginButton)
@@ -81,7 +82,7 @@ private extension LoginView {
         addSubview(loginButton)
         addSubview(signUpButton)
     }
-    
+
     // 오토레이아웃 설정
     func autoLayout() {
 //        kakaoLoginButton.snp.makeConstraints { make in
