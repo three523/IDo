a08b89ed7c77133687ded294a1a4e6b928a9319e
dohyun <thtree523@gmail.com>
2023-11-01 12:41:22 +0900

    [BUG] Club에 UserList를 저장하지 못하는 버그 수정

--- IDo/Page/NoticeBoard/NoticeHomeController.swift
+++ IDo/Page/NoticeBoard/NoticeHomeController.swift
@@ -15,7 +15,7 @@ class NoticeHomeController: UIViewController {
     var categoryData: String?
     var meetingIndex: Int?
     var club: Club
-    var signUpButtonUpdate: (() -> Void)?
+    var signUpButtonUpdate: ((Bool) -> Void)?
     let fbUserDatabaseManager: FirebaseUserDatabaseManager
     let clubImage: UIImage
     
@@ -73,10 +73,11 @@ class NoticeHomeController: UIViewController {
         self.fbUserDatabaseManager = fbUserDatabaseManager
         super.init(nibName: nil, bundle: nil)
         signUpButton.isHidden = isJoin
+        guard let myInfo = MyProfile.shared.myUserInfo else { return }
         self.fbUserDatabaseManager.update = { [weak self] in
-            guard let self else { return }
-            guard let myClubList = self.fbUserDatabaseManager.model?.myClubList else { return }
-            self.signUpButton.isHidden = myClubList.contains(where: { $0.id == self.club.id })
+            guard let userInfo = MyProfile.shared.myUserInfo,
+                  let joinUserList = self?.fbUserDatabaseManager.model?.userList else { return }
+            self?.signUpButton.isHidden = joinUserList.contains(where: { $0.id == userInfo.id })
         }
     }
     
