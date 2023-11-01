ef8044e0686ea3fc827f5f20552d13b956cbb30b
KangJiHun1028 <minsakr10@naver.com>
2023-11-01 18:03:31 +0900

    [FEAT]: Text Field 선택 시 다른 필드가 가려지는 경우를 대비해 화면을 이동시키게 구현

--- IDo/Page/SignUpPage/SignUpViewController.swift
+++ IDo/Page/SignUpPage/SignUpViewController.swift
@@ -135,7 +135,7 @@ final class SignUpViewController: UIViewController {
     private let emailAuthorizationButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("인증", for: .normal)
-        btn.setTitleColor(UIColor(color: .text2), for: .normal)
+        btn.setTitleColor(UIColor(color: .white), for: .normal)
         btn.backgroundColor = UIColor(color: .contentPrimary)
         btn.layer.cornerRadius = 5
         return btn
@@ -144,7 +144,7 @@ final class SignUpViewController: UIViewController {
     private let authenticationNumberButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("확인", for: .normal)
-        btn.setTitleColor(UIColor(color: .text2), for: .normal)
+        btn.setTitleColor(UIColor(color: .white), for: .normal)
         btn.backgroundColor = UIColor(color: .contentPrimary)
         btn.layer.cornerRadius = 5
         return btn
@@ -181,6 +181,7 @@ private extension SignUpViewController {
         addViews()
         autolayoutSetup()
         setupButton()
+        setupKeyboardEvent()
         passwordTextField.delegate = self
         passwordConfirmTextField.delegate = self
     }
@@ -393,6 +394,46 @@ private extension SignUpViewController {
         }
     }
     
+    func setupKeyboardEvent() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        // keyboardFrame: 현재 동작하고 있는 이벤트에서 키보드의 frame을 받아옴
+        // currentTextField: 현재 응답을 받고있는 UITextField를 알아냅니다.
+        guard let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentResponder as? UITextField else { return }
+        
+        // Y축으로 키보드의 상단 위치
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        // 현재 선택한 텍스트 필드의 Frame 값
+        let convertedTextFieldFrame = view.convert(currentTextField.frame,
+                                                   from: currentTextField.superview)
+        // Y축으로 현재 텍스트 필드의 하단 위치
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+        
+        // Y축으로 텍스트필드 하단 위치가 키보드 상단 위치보다 클 때 (즉, 텍스트필드가 키보드에 가려질 때가 되겠죠!)
+        if textFieldBottomY > keyboardTopY {
+            let textFieldTopY = convertedTextFieldFrame.origin.y
+            // 노가다를 통해서 모든 기종에 적절한 크기를 설정함.
+            let newFrame = textFieldTopY - keyboardTopY / 1.6
+            view.frame.origin.y -= newFrame
+        }
+    }
+
+    @objc func keyboardWillHide(_ sender: Notification) {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+
     @objc func addSMTPButton() {
         guard let emailText = emailTextField.text,
               !emailText.isEmpty,
@@ -550,5 +591,50 @@ extension SignUpViewController: UITextFieldDelegate {
             passwordConfirmErrorLabel.isHidden = false
             passwordConfirmErrorLabel.text = "비밀번호가 일치하지 않습니다."
         }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == emailTextField {
+            view.frame.origin.y = 0
+               
+        } else if textField == passwordTextField || textField == passwordConfirmTextField || textField == authenticationNumberTextField {
+            // 부드러운 효과를 위해 애니메이션 처리
+            UIView.animate(withDuration: 0.3) {
+                let transform = CGAffineTransform(translationX: 0, y: -100)
+                self.view.transform = transform
+            }
+        }
+    }
+       
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == emailTextField {
+            view.frame.origin.y = 0
+               
+        } else if textField == passwordTextField {
+            UIView.animate(withDuration: 0.3) {
+                let transform = CGAffineTransform(translationX: 0, y: 0)
+                self.view.transform = transform
+            }
+        }
+    }
+}
+
+extension UIResponder {
+    private enum Static {
+        weak static var responder: UIResponder?
+    }
+    
+    static var currentResponder: UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+    
+    @objc private func _trap() {
+        Static.responder = self
     }
 }
